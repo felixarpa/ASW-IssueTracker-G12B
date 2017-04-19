61e59498236233ea44d2248cc99b3e8be6b9a983
Guillermo Martinez <guillermomm1230@gmail.com>
2017-04-19 14:39:54 +0000

    Login amb Twitter aconseguit. Falta polir-ho molt

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :twitter, 'WauTZLszXdtZpT5CpBjYGGubx', 'n9d8fJ6roQ1X5SYOSh0VLcLhKqDPgdUqr8hPIX0P8wFRr9rtGF'
+    provider :twitter, Rails.application.secrets.twitter_api_key, Rails.application.secrets.twitter_api_secret
 end
