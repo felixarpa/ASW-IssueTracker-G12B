29b5cc54c64e3a43ea3e6f283fa7f72afc842631
Iván de Mingo Guerrero <ivandemingo2309@gmail.com>
2017-04-20 23:15:55 +0200

    Add types to Issue model and show view

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,2 +1,6 @@
 class Issue < ApplicationRecord
+  enum kind: { bug: '/images/issue_types/bug.svg',
+               enhancement: '/images/issue_types/enhancement.svg',
+               proposal: '/images/issue_types/suggestion.svg',
+               task: '/images/issue_types/task.svg' }
 end
