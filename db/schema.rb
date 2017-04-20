c3bd847bbb3ca1d4502c8c3c31f330250d802f92
Guillermo Martinez <guillermomm1230@gmail.com>
2017-04-20 22:14:03 +0000

    Login acabat. Falta intentar fer la interficie sense bootstrap

--- db/schema.rb
+++ db/schema.rb
@@ -15,17 +15,16 @@ ActiveRecord::Schema.define(version: 20170420135413) do
   create_table "issues", force: :cascade do |t|
     t.string   "title"
     t.string   "description"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
-    t.integer  "addigned_user"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "provider"
     t.string   "uid"
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "provider"
   end
 
 end
