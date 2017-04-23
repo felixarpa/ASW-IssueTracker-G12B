d5ea57da3ab5a1d6225ac9898a18a90ac8cea526
Guillermo Martinez <guillermomm1230@gmail.com>
2017-04-23 16:21:49 +0000

    Issues com a pagina principal i login funciona

--- db/schema.rb
+++ db/schema.rb
@@ -15,9 +15,9 @@ ActiveRecord::Schema.define(version: 20170423134647) do
   create_table "issues", force: :cascade do |t|
     t.string   "title"
     t.string   "description"
-    t.datetime "created_at",              null: false
-    t.datetime "updated_at",              null: false
-    t.integer  "kind",        default: 0
+    t.datetime "created_at",                                           null: false
+    t.datetime "updated_at",                                           null: false
+    t.string   "kind",        default: "/images/issue_types/task.svg"
     t.integer  "user_id"
   end
 
@@ -30,11 +30,11 @@ ActiveRecord::Schema.define(version: 20170423134647) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "provider"
     t.string   "uid"
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "provider"
   end
 
 end
