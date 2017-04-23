4881d259ec78702779a153482f7e0db1aa8c4d1a
Guillermo Martinez <guillermomm1230@gmail.com>
2017-04-23 10:38:43 +0000

    Login acabat

--- db/schema.rb
+++ db/schema.rb
@@ -15,8 +15,8 @@ ActiveRecord::Schema.define(version: 20170420224439) do
   create_table "issues", force: :cascade do |t|
     t.string   "title"
     t.string   "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",                                           null: false
+    t.datetime "updated_at",                                           null: false
     t.string   "kind",        default: "/images/issue_types/task.svg"
     t.integer  "user_id"
   end
