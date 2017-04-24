ff92aad3a1ddf921bb432ea33cab66ba1b265547
Felix Arribas Pardo <felixarpa@gmail.com>
2017-04-24 17:27:15 +0000

    #15 status controller

--- app/controllers/status_controller.rb
+++ app/controllers/status_controller.rb
@@ -1,6 +1,7 @@
 class WatchController < ApplicationController
   before_action :set_issue
 
+  # GET /status/toggle
   def toggle
     if @issue.status == 0
       @issue.status = 1
@@ -8,5 +9,11 @@ class WatchController < ApplicationController
       @issue.status = 0
     end
     redirect_to "/issues/#{ @issue.id }"
+  end
+
+  private
+
+  def set_issue
+    @issue = Issue.find_by(id: params[:id])
   end
 end
