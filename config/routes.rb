cdbf9f1d1541d8ae2a915a29fe698711119c6edc
Guillermo Martinez <guillermomm1230@gmail.com>
2017-04-24 23:37:56 +0000

    Afegir delete issue

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'home/profile' => 'home#profile'
   get 'auth/:provider/callback', to: "sessions#create"
   delete 'sign_out', to: "sessions#destroy", as: 'sign_out'
-  
+  delete 'delete_issue', to: "issues#destroy", as: 'delete_issue'
   resources :issues do
     member do
       resources :vote, only: :index
