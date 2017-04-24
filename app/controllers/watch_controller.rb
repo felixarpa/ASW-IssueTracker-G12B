688fcc4bff2f87dcdfced8fe58238f6aa40a3c90
Arnau Blanch <ArnauBlanch@users.noreply.github.com>
2017-04-24 16:19:11 +0200

    #11 route fix

--- app/controllers/watch_controller.rb
+++ app/controllers/watch_controller.rb
@@ -2,10 +2,10 @@ class WatchController < ApplicationController
   before_action :set_issue
 
   def index
-    if @issue.watchers.exists?(@current_user.id)
-      @issue.watchers.delete(@current_user)
+    if @issue.watchers.exists?(current_user.id)
+      @issue.watchers.delete(current_user)
     else
-      @issue.watchers << @current_user
+      @issue.watchers << current_user
     end
     redirect_to "/issues/#{ @issue.id }"
   end
--- db/migrate/20170424001656_add_index_watchers_table_unique.rb
+++ db/migrate/20170424001656_add_index_watchers_table_unique.rb
@@ -1,4 +1,4 @@
-class AddIndexWatchingTableUnique < ActiveRecord::Migration[5.0]
+class AddIndexWatchersTableUnique < ActiveRecord::Migration[5.0]
   def change
     add_index :table_watchers, [ :issue_id, :user_id ], unique: true
   end
