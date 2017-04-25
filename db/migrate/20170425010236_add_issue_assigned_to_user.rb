a380e6d726a4ddf76992ab49e7f1786bd7db3c5b
Iván de Mingo Guerrero <ivandemingo2309@gmail.com>
2017-04-25 10:09:52 +0200

    Fix migrations for heroku

--- db/migrate/20170425010236_add_issue_assigned_to_user.rb
+++ db/migrate/20170425010236_add_issue_assigned_to_user.rb
@@ -1,5 +1,5 @@
 class AddIssueAssignedToUser < ActiveRecord::Migration[5.0]
   def change
-    add_column :users, :issue_id, :belongs_to, index: true
+    add_column :users, :issue_id, :integer, index: true
   end
 end
--- db/migrate/20170425080144_change_issue_assigned_to_user.rb
+++
@@ -1,5 +0,0 @@
-class ChangeIssueAssignedToUser < ActiveRecord::Migration[5.0]
-  def change
-    change_column :users, :issue_id, :integer, index: true
-  end
-end
