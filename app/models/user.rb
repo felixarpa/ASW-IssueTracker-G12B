5eb19dfe36ac3c52907248ed737ac96f4f740bc7
Felix Arribas Pardo <felixarpa@gmail.com>
2017-04-25 09:18:14 +0000

    Merge user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,7 @@
 class User < ApplicationRecord
     has_many :issues
-<<<<<<< HEAD
     has_many :assigned_issues, :class_name => "Issue"
     has_many :comments
-=======
->>>>>>> d6e20898eac65abd031d80f770ec2e845c373f27
 
     def self.sign_in_from_omniauth(auth)
         find_by(provider: auth['provider'], uid: auth['uid']) || create_user_from_omniauth(auth)
