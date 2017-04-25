df3bc1d586207e2be60d9bec8a0d778a887773ac
Felix Arribas Pardo <felixarpa@gmail.com>
2017-04-25 09:20:19 +0000

    Borrar codi harcodejat

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -55,7 +55,7 @@ class IssuesController < ApplicationController
   # POST /issues.json
   def create
     @issue = Issue.new(issue_params)
-    #@issue.user = current_user
+    @issue.user = current_user
 
     respond_to do |format|
       if @issue.save
