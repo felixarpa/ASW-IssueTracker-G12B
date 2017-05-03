fc354391763544b4280ed16522c73a9b355c875e
Iván de Mingo Guerrero <ivandemingo2309@gmail.com>
2017-05-03 12:59:44 +0200

    Vote/unvote actions API support

--- app/controllers/vote_controller.rb
+++ app/controllers/vote_controller.rb
@@ -1,13 +1,39 @@
 class VoteController < ApplicationController
   before_action :set_issue
 
+  # POST /issues/:issue_id/vote
+  # POST /issues/:issue_id/vote.json
   def create
-    if @issue.votes.exists?(current_user.id)
-      @issue.votes.delete(current_user)
+    if @issue and current_user
+
+      message = 'Issue voted'
+      if @issue.votes.exists?(current_user.id)
+        @issue.votes.delete(current_user)
+        message = 'Issue unvoted'
+      else
+        @issue.votes << current_user
+      end
+      respond_to do |format|
+        format.html { redirect_to "/issues/#{ @issue.id }" }
+        format.json { render json: { issue: @issue }
+                                       .merge(message: message), status: :ok }
+      end
+
     else
-      @issue.votes << current_user
+
+      respond_to do |format|
+        if @issue.nil?
+          format.json { render json: { error:
+                                           { message: 'Issue does not exist' }
+          }, status: :not_found }
+        elsif current_user.nil?
+          format.json { render json: { error:
+                                           { message: 'Unauthorized' }
+          }, status: :unauthorized }
+        end
+      end
+
     end
-    redirect_to "/issues/#{ @issue.id }"
   end
 
   private
