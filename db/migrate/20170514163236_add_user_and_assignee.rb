341dc68cfab63a09ffae240cd5b9162c05229bc3
Arnau Blanch <ArnauBlanch@users.noreply.github.com>
2017-05-15 23:04:51 +0200

    Production DB fixed

--- db/migrate/20170514163236_add_user_and_assignee.rb
+++ db/migrate/20170514163236_add_user_and_assignee.rb
@@ -1,6 +1,6 @@
 class AddUserAndAssignee < ActiveRecord::Migration[5.0]
   def change
-    add_reference :issues, :user, foreign_key: true
-    add_reference :issues, :assignee, foreign_key: true
+    add_reference :issues, :user, foreign_key: false
+    add_reference :issues, :assignee, foreign_key: false
   end
 end
