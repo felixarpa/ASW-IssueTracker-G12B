f67524a87cf7e2a83e7262d60c5359226bda4177
Arnau Blanch <ArnauBlanch@users.noreply.github.com>
2017-05-16 00:49:50 +0200

    Algunes rutes eliminades

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
   root 'issues#index'
-  get 'home/profile' => 'home#profile'
   get 'auth/:provider/callback', to: "sessions#create"
   delete 'sign_out', to: "sessions#destroy", as: 'sign_out'
-  delete 'delete_issue', to: "issues#destroy", as: 'delete_issue'
 
   resources :issues do
     member do
