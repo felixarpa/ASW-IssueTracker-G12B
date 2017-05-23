a27c837d385ba479a9418c939120dfa8ee27d99b
Iván de Mingo Guerrero <ivandemingo2309@gmail.com>
2017-05-23 17:54:16 +0200

    Add id to issues response body

--- app/serializers/issue_serializer.rb
+++ app/serializers/issue_serializer.rb
@@ -1,5 +1,5 @@
 class IssueSerializer < ActiveModel::Serializer
-  attributes :title, :description, :created_at, :updated_at, :kind,
+  attributes :id, :title, :description, :created_at, :updated_at, :kind,
              :priority, :status, :votes
 
   def votes
