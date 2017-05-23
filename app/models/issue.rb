b73bfbbac4251d3ed244b6c2bfcd2b5c61f3797a
Iván de Mingo Guerrero <ivandemingo2309@gmail.com>
2017-05-24 01:35:41 +0200

    Issue status wontfix and duplicate added

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -3,7 +3,7 @@ class Issue < ApplicationRecord
   enum priority: { trivial: 0, minor: 1, major: 2, critical: 3, blocker: 4 }
   enum status: { new_issue: 'New', open: 'Open', on_hold: 'On hold',
                  invalid_issue: 'Invalid', resolved: 'Resolved',
-                 closed: 'Closed' }
+                 closed: 'Closed', wontfix: 'Wontfix', duplicate: 'Duplicate' }
   belongs_to :user, class_name: 'User', foreign_key: 'user_id'
   belongs_to :assignee, optional: true, class_name: 'User', foreign_key: 'assignee_id'
   has_and_belongs_to_many :votes, join_table: 'table_votes', class_name: 'User'
