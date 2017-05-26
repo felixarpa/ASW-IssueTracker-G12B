621e87ee18762750e1198c6b3591226e6ed8417d
Arnau Blanch <ArnauBlanch@users.noreply.github.com>
2017-05-27 01:19:56 +0200

    Trying to fix CORS issues with Fetch API

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,20 @@
 Rails.application.routes.draw do
-  root 'issues#index'
-  get 'auth/:provider/callback', to: "sessions#create"
-  delete 'sign_out', to: "sessions#destroy", as: 'sign_out'
+  root 'issues#index', via: :options
+  get 'auth/:provider/callback', to: "sessions#create", via: :options
+  delete 'sign_out', to: "sessions#destroy", as: 'sign_out', via: :options
 
   resources :issues do
     member do
-      resources :vote, only: :create
-      resources :watch, only: :create
-      resources :comments, except: [:edit, :new], param: :comment_id
+      resources :vote, only: :create, via: :options
+      resources :watch, only: :create, via: :options
+      resources :comments, except: [:edit, :new], param: :comment_id, via: :options
     end
   end
 
-  resources :users, only: [:index]
-  resources :me, only: [:index]
+  resources :users, only: [:index], via: :options
+  resources :me, only: [:index], via: :options
 
-  get 'issues/:id/attach', to: 'issues#attach', as: :attach_to_issue
-  resources :attached_files, only: :destroy
+  get 'issues/:id/attach', to: 'issues#attach', as: :attach_to_issue, via: :options
+  resources :attached_files, only: :destroy, via: :options
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
